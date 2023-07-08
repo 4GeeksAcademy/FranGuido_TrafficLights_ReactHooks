a0bb500ce0c0c84413dcb4830cbb667f431c4949
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-07-08 02:51:27 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,17 +6,23 @@ import React, {useState} from "react";
 //create your first component
 const Home = () => {
 
-	const [ lightColor, setlightColor] = useState("green");
+	const [ lightColor, setlightColor] = useState("");
 	return (
-		<div className="container text-center mt-5">
-			<div className="row justify-content-center">
+		<div className="container text-center">
+			<div className="pole"></div>
+			<div className="trafficLight row justify-content-center">
 				<div onClick={() => setlightColor("red")} className={"col-1 light  red" + ((lightColor === "red") ? " glowRed" : "")}></div>
 			
 				<div onClick={() => setlightColor("yellow")} className={"col-1 light  yellow" + ((lightColor === "yellow") ? " glowYellow" : "")}></div>
 		
 				<div onClick={() => setlightColor("green")} className={"col-1 light  green" + ((lightColor === "green") ? " glowGreen" : "")}></div>
 			</div>
+			<div className="row mt-4">
+				
+				<button onClick={() => setlightColor("")} className="btn btn-primary">Turn off</button>
+			</div>
 		</div>
+	
 	);
 };
 
